0b8cdf9f431a39b3aeefb9ba3b854b8b040324ad
Lorenzo Milesi <maxxer@yetopen.it>
2015-02-15 20:56:56 +0100

    Italian messages

---
+++ messages/it/attachments.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Translated to italian by Lorenzo Milesi <maxxer@yetopen.it>
+ */
+
+return [
+    'The model cannot be empty.' => 'Il modello non può essere vuoto.',
+    'The behavior FileBehavior has not been attached to the model.' => 'Il behavior FileBehavior non è stato associato al modello.',
+    'File name' => 'Nome file'
+];
