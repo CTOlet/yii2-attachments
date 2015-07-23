a9a635c3fd8d1766eb95174802415c1f000831c6
marsuboss <marsuboss@gmail.com>
2015-07-23 08:03:54 +0200

    Fixed

--- messages/fr/attachments.php
+++ messages/fr/attachments.php
@@ -4,6 +4,6 @@
  */
 return [
     'The model cannot be empty.' => 'Le modèle ne peut pas être vide',
-    'The behavior FileBehavior has not been attached to the model.' => 'Le comportement FileBehavior n'a pas été attaché au modèle.',
+    'The behavior FileBehavior has not been attached to the model.' => 'Le comportement FileBehavior n\'a pas été attaché au modèle.',
     'File name' => 'Nom de fichier'
 ];
