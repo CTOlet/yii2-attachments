69cdbe9667f9a10b4092c9432127ee6c81a2480e
rapita <n.i.rapita@gmail.com>
2016-11-30 18:51:19 +0200

    use utf8_unicode_ci
    Default set to table bad charset. In my case it setted `CHARSET = latin1` and it's bad for files with Cyrillic names.

--- src/migrations/m150127_040544_add_attachments.php
+++ src/migrations/m150127_040544_add_attachments.php
@@ -11,6 +11,12 @@ class m150127_040544_add_attachments extends Migration
 
     public function up()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        
         $this->createTable($this->getModule()->tableName, [
             'id' => Schema::TYPE_PK,
             'name' => Schema::TYPE_STRING . ' not null',
@@ -20,7 +26,7 @@ class m150127_040544_add_attachments extends Migration
             'size' => Schema::TYPE_INTEGER . ' not null',
             'type' => Schema::TYPE_STRING . ' not null',
             'mime' => Schema::TYPE_STRING . ' not null'
-        ]);
+        ], $tableOptions);
 
         $this->createIndex('file_model', $this->getModule()->tableName, 'model');
         $this->createIndex('file_item_id', $this->getModule()->tableName, 'itemId');
