8e7a02a076faad7c954728a859c2c22faee009da
zacksleo <zacksleo@gmail.com>
2018-11-15 09:49:31 +0800

    feat(i18n): add zh-CN & zh-TW messages

---
+++ src/messages/zh-CN/attachments.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Translated to French by MarsuBoss
+ */
+return [
+    'The model cannot be empty.' => '模型不能为空。',
+    'The behavior FileBehavior has not been attached to the model.' => 'FileBehavior行为尚未附加到模型。',
+    'File name' => '文件名'
+];
